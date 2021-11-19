f25f47289c2a9a83ec400ebcabf8207e60d8877a
gautama-bharadwaj <gautama98@gmail.com>
2021-11-19 14:40:18 -0800

    - Excel formatting - Validation to check if publication exists - Record execution time

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from scraper import Scraper
 from output import Output
+from datetime import datetime
 
 def run_module():
     scrape_data = Scraper('input/people_gscholar.xls').scrape()
     op = Output()
     clean_data = op.clean_data(scrape_data)
-    op.xlsoutput(clean_data, 'output/publications.xlsx')
+    op.xlsoutput(clean_data, 'output/UC Davis Bicycle Research.xlsx')
 
 
 if __name__ == "__main__":
+    start_time = datetime.now()
     run_module()
+    end_time = datetime.now()
+    print('Task execution time: {}'.format(end_time - start_time))
